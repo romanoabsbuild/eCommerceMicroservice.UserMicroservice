6d7d76c21bf1d316c4795c1409570a3b20d3f010
Romano Pedro <romano.pedro@absbuild.co.mz>
2025-02-19 21:05:45 +0200

    ApplicationUser fix accebility

--- eCommerce.Core/Models/ApplicationUser.cs
+++ eCommerce.Core/Models/ApplicationUser.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eCommerce.Core.Models
+﻿namespace eCommerce.Core.Models
 {
     /// <summary>
     /// Classe que representa um usuário da aplicação e é o modelo que armazena os dados do usuário
     /// </summary>
-    class ApplicationUser
+    public class ApplicationUser
     {
         public Guid UserID { get; set; }
         public string? Email { get; set; }
